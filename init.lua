df7e1d85d5d4b3ff903b9b4e14ccf53b2a55af4c
Marco Zahel <m.zahel@mugs.de>
2024-03-24 15:06:29 +0100

    Added autocompletion and lualine to native nvim config

--- init.lua
+++ init.lua
@@ -1,31 +1,31 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Enable highlighting on yank
 -- see https://github.com/vscode-neovim/vscode-neovim/issues/856
-vim.cmd [[
+vim.cmd([[
 augroup highlight_yank
     autocmd!
     autocmd TextYankPost * silent! lua vim.highlight.on_yank { higroup="IncSearch", timeout=200 }
 augroup END
-]]
+]])
 
 -- VIM OPTIONS
 -- use system clipboard
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 --easier seaching
 vim.o.ignorecase = true
 vim.o.smartcase = true
@@ -36,9 +36,14 @@ vim.o.relativenumber = true
 -- SET UP KEYBINDS
 -- VSCode functions
 if vim.g.vscode then
-  require 'vscode'
+	require("vscode")
 else
-  require("lazy").setup({
-    { import = 'plugins' }
-  })
+	-- diagnostic messages
+	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous dagnostic message" })
+	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic message" })
+	vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic error messages" })
+
+	require("lazy").setup({
+		{ import = "plugins" },
+	})
 end
